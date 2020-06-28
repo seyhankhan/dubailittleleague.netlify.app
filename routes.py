a48db4c665ff56864a697063ae2eb0b326a33976
Seyhan Van Khan <44237992+seyhanvankhan@users.noreply.github.com>
2020-06-28 17:24:46 +0400

    Added backend

---
+++ routes.py
@@ -0,0 +1,30 @@
+############## Seyhan Van Khan & Alizeh Khan
+############## Dubai Little League
+############## June 2020
+############## dubailittleleague.com
+
+################################ IMPORT MODULES ################################
+
+from flask import Flask, render_template, redirect, request, session, url_for
+
+################################### INIT APP ###################################
+
+
+app = Flask(__name__)
+app.secret_key = "stubblyainslieprocrastination"
+
+
+################################### HOME PAGE ##################################
+
+
+@app.route('/')
+def index():
+	return render_template('index.html',
+						   session_username=session['username'] if 'username' in session else None)
+
+
+#################################### APP RUN ###################################
+
+
+if __name__ == "__main__":
+    app.run()
