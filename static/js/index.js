084530473e645314f3a26ddc538c4227f525e7b0
Seyhan Van Khan <44237992+seyhanvankhan@users.noreply.github.com>
2020-06-30 00:25:48 +0400

    Scrolling stuff

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,8 @@
 /* ######################## JavaScript for index.html ####################### */
 window.onscroll = function() {
-  console.log(window.pageYOffset);
-
   var nav = document.getElementById('navbar');
-  if (window.pageYOffset/document.documentElement.clientHeight > 0.30) {
-    nav.style.backgroundColor = rgba(35, 31, 32, 0.5);
-  } else {
-    nav.style.backgroundColor = "";
-  }
+
+  ratio = (window.pageYOffset/document.documentElement.clientHeight) / 0.3527;
+  nav.style.backgroundColor = "rgba(35,31,32," + ((ratio < 1 ? ratio : 1) * 0.7).toString() + ")";
+  nav.style.marginTop = (20 > window.pageYOffset ? (20 - window.pageYOffset).toString() : '0') + 'px';
 }
