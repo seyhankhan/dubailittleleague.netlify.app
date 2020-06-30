815cdddd8a8654e1fe3c5bae47737659efc18b80
Seyhan Van Khan <44237992+seyhanvankhan@users.noreply.github.com>
2020-06-30 17:35:38 +0400

    Templatin

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,6 @@
 /* ######################## JavaScript for index.html ####################### */
 window.onscroll = function() {
-  maximumTransparency = 0.8;
+  maximumTransparency = 0.7;
   heightMaxPercentage = 0.3527;
   startingMarginTop = 20;
   var nav = document.getElementById('navbar');
@@ -8,4 +8,9 @@ window.onscroll = function() {
   ratio = (window.pageYOffset/document.documentElement.clientHeight) / heightMaxPercentage;
   nav.style.backgroundColor = "rgba(35,31,32," + ((ratio < 1 ? ratio : 1) * maximumTransparency).toString() + ")";
   nav.style.marginTop = (startingMarginTop > window.pageYOffset ? (startingMarginTop - window.pageYOffset).toString() : '0') + 'px';
+}
+function goDownToContent() {
+    $('html, body').animate({
+        scrollTop: $("#downArrow").offset().top + $("#downArrow").height()
+    }, 1000 ); //in ms
 }
