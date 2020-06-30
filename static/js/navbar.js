779bd9da52bf14f97679e02206455de4811c8149
Seyhan Van Khan <44237992+seyhanvankhan@users.noreply.github.com>
2020-06-30 18:12:33 +0400

    Update navbar.js

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,12 @@
-/* ######################## JavaScript for index.html ####################### */
-window.onscroll = function() {
+/* ######################## JavaScript for index.html ####################### */\
+function goDownToContent() {
   scrollDownTime = 800; // in milliseconds
+    $('html, body').animate({
+        scrollTop: $("#downArrow").offset().top + $("#downArrow").height()
+    }, scrollDownTime );
+}
+
+window.onscroll = function() {
   maximumTransparency = 0.7;
   heightMaxPercentage = 0.3527;
   startingMarginTop = 20;
@@ -9,9 +15,4 @@ window.onscroll = function() {
   ratio = (window.pageYOffset/document.documentElement.clientHeight) / heightMaxPercentage;
   nav.style.backgroundColor = "rgba(35,31,32," + ((ratio < 1 ? ratio : 1) * maximumTransparency).toString() + ")";
   nav.style.marginTop = (startingMarginTop > window.pageYOffset ? (startingMarginTop - window.pageYOffset).toString() : '0') + 'px';
-}
-function goDownToContent() {
-    $('html, body').animate({
-        scrollTop: $("#downArrow").offset().top + $("#downArrow").height()
-    }, scrollDownTime );
 }
