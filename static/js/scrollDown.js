a75d16e08c7e7abf3f58788c94d33e1c6ebd688f
Seyhan Van Khan <44237992+seyhanvankhan@users.noreply.github.com>
2020-07-10 19:03:22 +0400

    EMAIL COPY BUTTON WORKS :D

--- static/js/scrollDown.js
+++ static/js/scrollDown.js
@@ -19,19 +19,36 @@ function goDownToContent() {
 
 const emailSVG = document.getElementById('email-svg');
 const emailPopup = document.getElementById('email-popup');
+const copySVG = document.getElementById('copy-svg');
+const emailAddressText = document.getElementById('email-address');
 
 emailSVG.addEventListener('mouseenter', e => {
-  emailPopup.style.display = '';
+  emailPopup.style.display = 'flex';
 });
 emailPopup.addEventListener('mouseleave', e => {
   emailPopup.style.display = 'none';
+  copySVG.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-copy"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>';
+  window.getSelection().removeAllRanges();
 });
+copySVG.addEventListener('click', e => {
+  //highlight the text
+  var range = document.createRange();
+  range.selectNode(emailAddressText);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(range);
+  // copy the text
+  document.execCommand('copy');
+  // turn it into a checkmark
+  copySVG.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>';
+  console.log('Email is copied!');
+});
+
 
 const phoneSVG = document.getElementById('phone-svg');
 const phonePopup = document.getElementById('phone-popup');
 
 phoneSVG.addEventListener('mouseenter', e => {
-  phonePopup.style.display = '';
+  phonePopup.style.display = 'flex';
 });
 phonePopup.addEventListener('mouseleave', e => {
   phonePopup.style.display = 'none';
